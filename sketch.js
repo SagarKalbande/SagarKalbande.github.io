fcead9d88163b9556ddde7e172890bcf79d59d4c
Sagar Kalbande <sagarkalbande1993@gmail.com>
2017-01-27 02:49:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,60 +1,21 @@
-var stars = []
-
-var speed;
-
 function setup() {
-  createCanvas(600, 600);
-  for (var i = 0; i < 800; i++) {
-    stars[i] = new Star();
-  }
+
+  createCanvas(700,700);
+  background(255,150,120)
+
 }
 
 function draw() {
-  speed = map(mouseX, 0, width, 0, 50);
-  background(0);
-  translate(width / 2, height / 2);
-  for (var i = 0; i < stars.length; i++) {
-    stars[i].update();
-    stars[i].show();
+	noStroke();
+	
+  if ((mouseX<400)&&(mouseX>200)) {
+    fill(0,0,255,50);
+  } 
+  else if ((mouseX<600)&&(mouseX>400)){
+    fill(255,0,0,50);
   }
-}
-
-// Daniel Shiffman
-// Code for: https://youtu.be/17WoOqgXsRM
-
-function Star() {
-  this.x = random(-width, width);
-  this.y = random(-height, height);
-  this.z = random(width);
-  this.pz = this.z;
-
-  this.update = function() {
-    this.z = this.z - speed;
-    if (this.z < 1) {
-      this.z = width;
-      this.x = random(-width, width);
-      this.y = random(-height, height);
-      this.pz = this.z;
-    }
+  else if ((mouseX<700)&&(mouseX>600)){
+    fill(0,255,0,50);
   }
-
-  this.show = function() {
-    fill(255);
-    noStroke();
-
-    var sx = map(this.x / this.z, 0, 1, 0, width);
-    var sy = map(this.y / this.z, 0, 1, 0, height);
-
-    var r = map(this.z, 0, width, 16, 0);
-    ellipse(sx, sy, r, r);
-
-    var px = map(this.x / this.pz, 0, 1, 0, width);
-    var py = map(this.y / this.pz, 0, 1, 0, height);
-
-    this.pz = this.z;
-
-    stroke(255);
-    line(px, py, sx, sy);
-
-  }
+  ellipse(mouseX, mouseY, 20, 20);
 }
