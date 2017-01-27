3956ec04b15b03fb8684537b2db68a5e31ff7d70
Sagar Kalbande <sagarkalbande1993@gmail.com>
2017-01-27 22:41:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,12 @@
 function setup() {
 
-  createCanvas(600,600);
-  background(255,150,120)
+  createCanvas(1200,1200);
+  background(255,255,255)
 
 }
 
 function draw() {
 	noStroke();
-	
-  if ((mouseY<200)&&(mouseY>00)) {
-    fill(255,153,51,50);
-  } 
-  else if ((mouseY<400)&&(mouseY>200)){
-    fill(255,255,255,50);
-  }
-  else if ((mouseY<600)&&(mouseY>400)){
-    fill(0,255,0,50);
-  }
-  ellipse(mouseX, mouseY, 200, 200);
+	fill(0,0,0);
+	ellipse(mouseX, mouseY, 05, 5);
 }
